a7d970a39111c1b2f01665d4e500e87165db7e57
Hayk Jhangiryan <hayk.jhangiryan@auto1.com>
2017-03-20 12:33:22 +0100

    formatting

--- src/com/goeuro/main/Main.java
+++ src/com/goeuro/main/Main.java
@@ -12,45 +12,46 @@ import static com.goeuro.Constants.*;
  * Created by hayk on 1/23/15.
  */
 public class Main {
-	public static void main(String[] args) {
-		System.out.println(WELCOME_MSG);
-		String cityName = "";
-		if (args.length > 0) {
-			cityName = args[0];
-		}
-		try (CommandLineInfoHelper.Cmd cmd = CommandLineInfoHelper.getCMD()) {
-			while (cityName.isEmpty()) {
-				cityName = cmd.read(READ_CITY_NAME_NOTIFICATION);
-			}
-			while (!cityName.equalsIgnoreCase(EXIT)) {
-				process(cityName);
-				cityName = cmd.read(READ_CITY_NAME_NOTIFICATION);
-			}
-		} catch (Exception e) {
-			System.out.printf(CMD_READ_FAILURE_MSG, e.getMessage());
-		}
-	}
+    public static void main(String[] args) {
 
-	private static void process(String cityName) {
-		String url = GO_EURO_API_URL + cityName;
-		System.out.printf(LOCATION_INFO_LOADING_MSG, cityName, url);
-		Object result = HttpHelper.doGet(url);
-		LocationInfoList locationInfoList = null;
-		try {
-			if (result instanceof Throwable) {
-				throw (Throwable) result;
-			} else {
-				locationInfoList = JsonHelper.decode((String) result, LocationInfoList.class);
-			}
-		} catch (Throwable t) {
-			System.out.println(DATA_FAILURE_MSG);
-		}
-		if (locationInfoList == null || locationInfoList.isEmpty()) {
-			System.out.println(EMPTY_RESPONSE_MSG);
-		} else {
-			System.out.println(GENERATING_CSV_MSG);
-			String fileName = CSVHelper.exportData(locationInfoList, cityName);
-			System.out.printf(FILE_SUCCESSFUL_SAVE_MSG, fileName);
-		}
-	}
+        System.out.println(WELCOME_MSG);
+        String cityName = "";
+        if (args.length > 0) {
+            cityName = args[0];
+        }
+        try (CommandLineInfoHelper.Cmd cmd = CommandLineInfoHelper.getCMD()) {
+            while (cityName.isEmpty()) {
+                cityName = cmd.read(READ_CITY_NAME_NOTIFICATION);
+            }
+            while (!cityName.equalsIgnoreCase(EXIT)) {
+                process(cityName);
+                cityName = cmd.read(READ_CITY_NAME_NOTIFICATION);
+            }
+        } catch (Exception e) {
+            System.out.printf(CMD_READ_FAILURE_MSG, e.getMessage());
+        }
+    }
+
+    private static void process(String cityName) {
+        String url = GO_EURO_API_URL + cityName;
+        System.out.printf(LOCATION_INFO_LOADING_MSG, cityName, url);
+        Object result = HttpHelper.doGet(url);
+        LocationInfoList locationInfoList = null;
+        try {
+            if (result instanceof Throwable) {
+                throw (Throwable) result;
+            } else {
+                locationInfoList = JsonHelper.decode((String) result, LocationInfoList.class);
+            }
+        } catch (Throwable t) {
+            System.out.println(DATA_FAILURE_MSG);
+        }
+        if (locationInfoList == null || locationInfoList.isEmpty()) {
+            System.out.println(EMPTY_RESPONSE_MSG);
+        } else {
+            System.out.println(GENERATING_CSV_MSG);
+            String fileName = CSVHelper.exportData(locationInfoList, cityName);
+            System.out.printf(FILE_SUCCESSFUL_SAVE_MSG, fileName);
+        }
+    }
 }
